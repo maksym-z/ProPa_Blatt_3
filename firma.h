00e502ea82461fd03fc50834251d39c62c8fb2b6
zinnatmm <zinnatmm@gspc12.informatik.uni-stuttgart.de>
2017-05-10 14:44:29 +0200

    first commit

---
+++ firma.h
@@ -0,0 +1,23 @@
+uint64_t gewinn (const int64_t *const d, const size_t n) {
+  // Implementieren Sie diese Funktion
+  int64_t i, current_max, max_ending_here;
+  
+  const int64_t *ptr;
+  ptr = d;
+  current_max = 0;
+  max_ending_here = 0;
+  startIndex = 0;
+  endIndex = 0;
+  
+  for(i = 0; i < n; i++) {
+    max_ending_here += *ptr;
+    if (max_ending_here < 0) {
+      max_ending_here = 0;
+    }
+    if (current_max < max_ending_here) {
+      current_max = max_ending_here;
+    }
+    ptr++;
+  }
+  return current_max;
+}
