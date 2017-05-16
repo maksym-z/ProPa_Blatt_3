bc4572a2b7c6778b0b80070e518ffa504a555264
ODK-Bn <tea-with-lemon@yandex.ru>
2017-05-17 01:03:11 +0200

    removed unused vars

--- firma.h
+++ firma.h
@@ -1,13 +1,11 @@
 uint64_t gewinn (const int64_t *const d, const size_t n) {
   // Implementieren Sie diese Funktion
-  int64_t i, current_max, max_ending_here, startIndex, endIndex;
+  int64_t i, current_max, max_ending_here;
   
   const int64_t *ptr;
   ptr = d;
   current_max = 0;
   max_ending_here = 0;
-  startIndex = 0;
-  endIndex = 0;
   
   for(i = 0; i < n; i++) {
     max_ending_here += *ptr;
